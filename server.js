9ccc77778668ed983f7040addbea45944cfac13b
Jay McDoniel <jay.mcdoniel@ad.infosys.com>
2018-03-14 11:58:26 -0700

    Removed database conection for the moment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
-const db = require('./db/index');
+//const db = require('./db/index');
 
 const PORT = process.env.PORT || 4200;
 
