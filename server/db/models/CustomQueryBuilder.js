a586814b1562a8f3010631fbac2a396b4e2bcc39
PerfectOrphan31 <jmcdo29@gmail.com>
2018-08-02 10:31:31 -0700

    Got it to work with modelInstances, not just overall models!

--- server/db/models/CustomQueryBuilder.js
+++ server/db/models/CustomQueryBuilder.js
@@ -4,7 +4,16 @@ class MyQueryBuilder extends QueryBuilder {
 
   upsert(model) {
     if (model.id && model.id !== null) {
-      return this.update(model).where({id: model.id}).returning('id');
+      return new Promise((resolve, reject) => {
+        resolve(this.findById(model.id));
+      })
+      .then(modelInstance => {
+        return modelInstance.$query().update(model).returning('id');
+      })
+      .catch(err => {
+        return err;
+      })
+      //return this.update(model).where({id: model.id}).returning('id');
     } else {
       return this.insert(model).returning('id');
     }
