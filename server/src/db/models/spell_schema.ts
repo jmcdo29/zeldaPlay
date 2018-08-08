b8d7f66e21b83ab9be538916314623681616ff40
PerfectOrphan31 <jmcdo29@gmail.com>
2018-08-08 16:15:30 -0700

    Added spell interface and class documentation.

--- server/src/db/models/spell_schema.ts
+++ server/src/db/models/spell_schema.ts
@@ -1,7 +1,23 @@
-// TODO: Add JSDoc documentation for file.
 import { Model, QueryBuilder } from 'objection';
 import { makeId, checkNull } from '../../utils/utils';
+import { SpellInterface } from '../../interfaces/spellInterface';
 
+/**
+ * @extends Model
+ * @prop {string} tableName - spell
+ * @prop {string} id
+ * @prop {string} last_modified - time string of the last modification
+ * @prop {string} name - name of the spell
+ * @prop {string} effect - what the spell does
+ * @prop {number} mp_use - how much mp the spell costs
+ * @prop {number} damage - definition of which dice to use
+ * @prop {number} number_of_hit - how many times to roll the damage dice
+ * @prop {string} modifer - which modifier to add to the spell
+ * @prop {string} diety - which diety power the spell draws from, Nayru, Farore, or Din
+ * @prop {boolean} use_diety - if the spell should fully draw from diety power or use modifier too
+ * @prop {string} last_modified_by - the id of the user who last modified the spell
+ * @prop {string} character_id - id of the character the spell belongs to
+ */
 export class Spell extends Model {
   static tableName = 'spell';
 
@@ -12,7 +28,7 @@ export class Spell extends Model {
   mp_use: number;
   damage: number;
   number_of_hit: number;
-  modifier: string;
+  modifier?: string;
   diety: string;
   use_diety = false;
   last_modified_by: string;
@@ -26,7 +42,7 @@ export class Spell extends Model {
     }
   }
 
-  constructor(id?, chId?, values?) {
+  constructor(id?: string, chId?: string, values?: SpellInterface) {
     super();
     if (id && chId && values) {
       this.id = <string>checkNull(values.id);
---
+++ server/src/interfaces/spellInterface.ts
@@ -0,0 +1,22 @@
+/**
+ * @prop {string} [id]
+ * @prop {string} name - what the spell is called
+ * @prop {string} effect - what the spell does.
+ * @prop {number} mpUse - how much mp the spell costs
+ * @prop {string} [modifier] - modifier to use when calculating damage, if any
+ * @prop {string} diety - which diety the spell falls under, Nayru, Farore, or Din
+ * @prop {boolean} useDiety - if diety should be used instead of modifier
+ * @prop {number} damage - the kind of dice to roll for casting the spell
+ * @prop {number} multiplier - how many dice the spell should roll
+ */
+export interface SpellInterface {
+  id?: string;
+  name: string;
+  effect: string;
+  mpUse: number;
+  modifier?: string;
+  diety: string;
+  useDiety: boolean;
+  damage: number;
+  multiplier: number;
+}
