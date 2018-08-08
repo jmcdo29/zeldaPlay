1568098c68d190224d61577b09a3577c855c9133
PerfectOrphan31 <jmcdo29@gmail.com>
2018-08-08 15:02:29 -0700

    Added save interface and documentation for saves class and interface.

--- server/src/db/models/save_schema.ts
+++ server/src/db/models/save_schema.ts
@@ -1,14 +1,25 @@
 // TODO: Add JSDoc documentation for file.
 import { Model, QueryBuilder } from 'objection';
 import { makeId, checkNull } from '../../utils/utils';
+import { SaveInterface } from '../../interfaces/saveInterface';
 
+/**
+ * @extends {Model}
+ * @prop {string} id
+ * @prop {string} last_modified - string containing the last modified datetime
+ * @prop {string} character_id - the character id of the character the save belongs to
+ * @prop {string} racial_bonus - the possible bonus to the roll due to the character's race.
+ * @prop {string} name - the name of the saving throw - will be Fortitude, Reflex, or Will
+ * @prop {string} modifier - the name of the modifier to use - will be Dexterity, Constitution, or Wisdom
+ * @prop {string} last_modified_by - the id of the user who last modified the Save
+ */
 export class Save extends Model {
   static tableName = 'saving_throw';
 
   id: string;
   last_modified: string;
   character_id: string;
-  racial_bonus: number | string;
+  racial_bonus: number;
   name: string;
   modifier: string;
   last_modified_by: string;
@@ -21,7 +32,7 @@ export class Save extends Model {
     }
   }
 
-  constructor(id, chId, values) {
+  constructor(id: string, chId: string, values: SaveInterface) {
     super();
     if (id && chId && values) {
       this.id = <string>checkNull(values.id);
