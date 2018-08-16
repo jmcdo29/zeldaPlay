67756bc635902e012b325564bcdd806b8fac2fff
PerfectOrphan31 <jmcdo29@gmail.com>
2018-08-16 11:55:20 -0700

    Changed all migrations to js files because of issues with Knex and Typescript.

--- server/src/db/knexfile.ts
+++ server/src/db/knexfile.ts
@@ -1,11 +1,11 @@
 import * as dotenv from 'dotenv';
 import * as pg from 'pg';
 
-dotenv.config({ path: '../../.env' });
+dotenv.config({ path: '../../../.env' });
 
 pg.defaults.ssl = true;
 
-export const connectionConfig = {
+module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL
 };
