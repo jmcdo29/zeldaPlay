249bb89987a671b14daf69fe23367c2b8cc00593
PerfectOrphan31 <jmcdo29@gmail.com>
2018-08-23 21:10:28 -0700

    Added lines to ignore code coverage for www.ts file. Added a personal delete test file.

--- jest.config.js
+++ jest.config.js
@@ -54,7 +54,8 @@ module.exports = {
     "!**/test.ts",
     "!**/server.ts",
     "!**/jestGlobalMocks.ts",
-    "!**/dbConnection.ts"
+    "!**/dbConnection.ts",
+    "!**/www.ts"
   ],
   coverageReporters: ['lcov', 'text']
 };
