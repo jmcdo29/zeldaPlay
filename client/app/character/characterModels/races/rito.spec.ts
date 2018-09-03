11854a48031654fb550ada0ac98e8f1fab210d81
PerfectOrphan31 <jmcdo29@gmail.com>
2018-09-02 20:58:09 -0700

    Fixed the Rito class.

--- client/app/character/characterModels/races/rito.spec.ts
+++ client/app/character/characterModels/races/rito.spec.ts
@@ -3,19 +3,19 @@ import { Rito } from './rito';
 test('should create a Rito', () => {
   const myRito = new Rito();
   expect(myRito).toBeTruthy();
-  expect(myRito.race).toBe('Rito');
+  expect(myRito.getRace()).toBe('Rito');
 });
 test('should create a Sharp Eye Rito', () => {
   const myRito = new Rito('Sharp Eye');
   expect(myRito).toBeTruthy();
-  expect(myRito.race).toBe('Rito');
-  expect(myRito.subRace).toBe('Sharp Eye');
-  expect(myRito.attributes[3].value).toBe(9);
+  expect(myRito.getRace()).toBe('Rito');
+  expect(myRito.getSubRace()).toBe('Sharp Eye');
+  expect(myRito.getAttributes()[3].getValue()).toBe(9);
 });
 test('should create a Sharp Tongue Rito', () => {
   const myRito = new Rito('Sharp Tongue');
   expect(myRito).toBeTruthy();
-  expect(myRito.race).toBe('Rito');
-  expect(myRito.subRace).toBe('Sharp Tongue');
-  expect(myRito.attributes[5].value).toBe(9);
+  expect(myRito.getRace()).toBe('Rito');
+  expect(myRito.getSubRace()).toBe('Sharp Tongue');
+  expect(myRito.getAttributes()[5].getValue()).toBe(9);
 });
