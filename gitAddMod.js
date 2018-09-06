39e8b06e5e6b7bbdc522e10f208d604b3474ed63
PerfectOrphan31 <jmcdo29@gmail.com>
2018-09-06 01:58:08 -0700

    Modified custom git add script and added .snyk policy

--- gitAddMod.js
+++ gitAddMod.js
@@ -12,17 +12,15 @@ exec('git diff --cached --name-status', (error, stdout, stderr) => {
     filesToUpdate.push(file.trim());
   });
   let updateString = '';
-  let index = 0;
   for (const file of filesToUpdate) {
     if (
-      index !== filesToUpdate.length - 1 &&
+      file &&
       (!file.split('\t')[0].includes('D') || !file.split('\t')[0].includes('U'))
     ) {
       updateString += file.split('\t')[1].trim() + ' ';
     }
-    index++;
   }
-  // console.log(`git add ${updateString}`);
+  console.log(`git add ${updateString}`);
   exec(`git add ${updateString}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error ${error}.`);
