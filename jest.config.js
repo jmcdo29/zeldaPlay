d7cb7a96a759cfa53a5de9611152c3fce0b9b0c0
PerfectOrphan31 <jmcdo29@gmail.com>
2018-09-27 09:03:15 -0700

    Ran prettier for ts, css, and scss files. Want to move all CSS files to SCSS when I have the time.

--- jest.config.js
+++ jest.config.js
@@ -1,62 +1,51 @@
 module.exports = {
-  roots: [
-    '<rootDir>/client',
-    '<rootDir>/server'
-  ],
+  roots: ['<rootDir>/client', '<rootDir>/server'],
   cacheDirectory: '<rootDir>/tmp/',
   preset: 'jest-preset-angular',
   globals: {
-    "ts-jest": {
-      "tsConfigFile": "./tsconfig.json"
+    'ts-jest': {
+      tsConfigFile: './tsconfig.json'
     },
-    "__TRANSFORM_HTML__": true
+    __TRANSFORM_HTML__: true
   },
   transform: {
-    "^.+\\.(ts|js|html)$": "<rootDir>/node_modules/jest-preset-angular/preprocessor.js"
+    '^.+\\.(ts|js|html)$':
+      '<rootDir>/node_modules/jest-preset-angular/preprocessor.js'
   },
-  testMatch: [
-    "**/*.spec.ts",
-  ],
-  moduleFileExtensions: [
-    "ts",
-    "js",
-    "json",
-    "html"
-  ],
+  testMatch: ['**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'html'],
   moduleNameMapper: {
-    "^client/(.*)": "<rootDir>/client/$1",
-    "^server/(.*)": "<rootDir>/server/$1"
+    '^client/(.*)': '<rootDir>/client/$1',
+    '^server/(.*)': '<rootDir>/server/$1'
   },
-  transformIgnorePatterns: [
-    "node_modules/(?!@ngrx)"
-  ],
+  transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
   snapshotSerializers: [
-    "<rootDir>/node_modules/jest-preset-angular/AngularSnapshotSerializer.js",
-    "<rootDir>/node_modules/jest-preset-angular/HTMLCommentSerializer.js"
+    '<rootDir>/node_modules/jest-preset-angular/AngularSnapshotSerializer.js',
+    '<rootDir>/node_modules/jest-preset-angular/HTMLCommentSerializer.js'
   ],
   setupTestFrameworkScriptFile: '<rootDir>/client/setupJest.ts',
   collectCoverage: true,
   collectCoverageFrom: [
-    "**/*.{js,ts}",
-    "!**/*.d.ts",
-    "!**/*.config.js",
-    "!**/node_modules/**",
-    "!**/coverage/**",
-    "!**/reference/**",
-    "!**/migrations/**",
-    "!**/seeds/**",
-    "!**/mocks/**",
-    "!**/*.json",
-    "!**/knexfile.ts",
-    "!**/sessionConf.ts",
-    "!**/main.ts",
-    "!**/polyfills.ts",
-    "!**/setupJest.ts",
-    "!**/test.ts",
-    "!**/server.ts",
-    "!**/jestGlobalMocks.ts",
-    "!**/dbConnection.ts",
-    "!**/www.ts"
+    '**/*.{js,ts}',
+    '!**/*.d.ts',
+    '!**/*.config.js',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!**/reference/**',
+    '!**/migrations/**',
+    '!**/seeds/**',
+    '!**/mocks/**',
+    '!**/*.json',
+    '!**/knexfile.ts',
+    '!**/sessionConf.ts',
+    '!**/main.ts',
+    '!**/polyfills.ts',
+    '!**/setupJest.ts',
+    '!**/test.ts',
+    '!**/server.ts',
+    '!**/jestGlobalMocks.ts',
+    '!**/dbConnection.ts',
+    '!**/www.ts'
   ],
   coverageReporters: ['lcov', 'text']
 };
