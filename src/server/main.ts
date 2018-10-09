8d07385829d4c25a27b7425a86d6ec7c5c27695b
PerfectOrphan31 <jmcdo29@gmail.com>
2018-10-09 12:29:06 -0700

    Added swagger module to automatically start creating swagger documentation.

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,3 +1,4 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { config } from 'dotenv';
 import * as path from 'path';
 config();
@@ -9,6 +10,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const rootPath = path.join(__dirname, '../client');
   app.useStaticAssets(rootPath);
+
+  const options = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT, () => {
     console.log(`Application stated on ${process.env.PORT}.`);
   });
