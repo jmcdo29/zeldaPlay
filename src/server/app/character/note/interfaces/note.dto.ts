08594b44366f4176a529d3d923a1b493a2aa1b03
PerfectOrphan31 <jmcdo29@gmail.com>
2018-10-09 12:41:25 -0700

    Added beginning of automated swagger documentation.

--- src/server/app/character/note/interfaces/note.dto.ts
+++ src/server/app/character/note/interfaces/note.dto.ts
@@ -1,6 +1,15 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class NoteDTO {
+  @ApiModelProperty()
   message: string;
+
+  @ApiModelProperty()
   time: string;
+
+  @ApiModelProperty()
   id?: string;
+
+  @ApiModelProperty()
   important: boolean;
 }
--- src/server/app/character/skill/interfaces/skill.dto.ts
+++ src/server/app/character/skill/interfaces/skill.dto.ts
@@ -1,11 +1,30 @@
+import { ApiModelProperty } from '@nestjs/swagger';
+
 export class SkillDTO {
+  @ApiModelProperty()
   id?: string;
+
+  @ApiModelProperty()
   name: string;
+
+  @ApiModelProperty()
   type: string;
+
+  @ApiModelProperty()
   ranks: number;
+
+  @ApiModelProperty()
   trained: boolean;
+
+  @ApiModelProperty()
   racial?: number;
+
+  @ApiModelProperty()
   misc?: number;
+
+  @ApiModelProperty()
   item?: number;
+
+  @ApiModelProperty()
   modifier?: string;
 }
