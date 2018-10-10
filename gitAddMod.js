1c5a7a9376083a2d45791ed8416708008e74ccf2
PerfectOrphan31 <jmcdo29@gmail.com>
2018-10-10 09:39:22 -0700

    Updated gitignore and moved the notification icon for when nodeifier is triggered. Also resized the icon.

--- gitAddMod.js
+++ gitAddMod.js
@@ -26,7 +26,7 @@ exec('git diff --cached --name-status', (error, stdout, stderr) => {
   notifier.notify({
     message: 'Finished adding files!',
     title: 'Adding From Script',
-    icon: './reference/notifIcon.png'
+    icon: './notifIcon.png'
   });
   exec(`git add ${updateString}`, (error, stdout, stderr) => {
     if (error) {
