d8453f1194bd5e129aaa182a30b94265e350e3c4
PerfectOrphan31 <jmcdo29@gmail.com>
2018-10-29 13:36:46 -0700

    Removed a promise reject and changed it to a throw Error, updated the readme to read Angular 2+ instead of 6+ and removed error codes as they are no longer used. Also updated swagger API Tag descriptions.

--- src/server/main.ts
+++ src/server/main.ts
@@ -26,12 +26,24 @@ async function bootstrap() {
       'MIT',
       'https://github.com/jmcdo29/zeldaPlay/blob/master/LICENSE'
     )
-    .addTag('character')
-    .addTag('note')
-    .addTag('spell')
-    .addTag('skill')
-    .addTag('weapon')
-    .addTag('user')
+    .addTag(
+      'character',
+      'APIs that specifically pertain to the character or character saves'
+    )
+    .addTag(
+      'note',
+      'APIs for getting the Notes of and adding new Notes for the character'
+    )
+    .addTag(
+      'spell',
+      'APIs for getting the Spells of and adding new Spells for the character'
+    )
+    .addTag('skill', 'APIs for getting new Skills for the character')
+    .addTag(
+      'weapon',
+      'APIs for getting the Spells of and adding new Weapons for the character'
+    )
+    .addTag('user', 'APIs for registering and logging in to the application')
     .addBearerAuth('Authorization', 'header')
     .build();
   const document = SwaggerModule.createDocument(app, options, {});
