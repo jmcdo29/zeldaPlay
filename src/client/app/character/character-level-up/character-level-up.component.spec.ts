f93bd6f39e30aca266b6d13c6bf00279cf390ab0
PerfectOrphan31 <jmcdo29@gmail.com>
2018-11-05 09:38:45 -0800

    Added angular-material to character-level-up and added button material to character-create.

--- src/client/app/character/character-level-up/character-level-up.component.spec.ts
+++ src/client/app/character/character-level-up/character-level-up.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
 import { Character } from '#Models/character';
+import { MaterialModule } from '#Shared/material/material.module';
 import { CharacterLevelUpComponent } from './character-level-up.component';
 
 describe('CharacterLevelUpComponent', () => {
@@ -10,7 +11,7 @@ describe('CharacterLevelUpComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [FormsModule],
+      imports: [FormsModule, MaterialModule],
       declarations: [CharacterLevelUpComponent]
     }).compileComponents();
   }));
