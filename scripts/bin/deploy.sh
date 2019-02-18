3cb694bcf7b85cf18d46156860d94e2baf23ebe3
Jay McDoniel <jmcdo29@gmail.com>
2019-02-18 10:57:30 -0800

    Updated script/bin/deploy

--- scripts/bin/deploy.sh
+++ scripts/bin/deploy.sh
@@ -2,7 +2,7 @@
 rev=$(git rev-parse --short HEAD)
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.com"
-git add ./dist
+git add ./dist package* angular.json
 git commit --no-verify -m "Auto deploy from travis $TRAVIS_BUILD_NUMBER: ${rev}"
 git remote add deploy "https://jmcdo29:$GH_TOKEN@github.com/jmcdo29/zeldaplay.git" &>dev/null
 git push deploy build &>dev/null
