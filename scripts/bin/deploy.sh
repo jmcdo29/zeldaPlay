8c958c4d9d5961c54860f934db4a1711ab8ae5df
Jay McDoniel <jmcdo29@gmail.com>
2019-02-18 12:00:40 -0800

    Attempting to build in the deploy script.

--- scripts/bin/deploy.sh
+++ scripts/bin/deploy.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 rev=$(git rev-parse --short HEAD)
+npm run build
 ls
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.com"
-git add ./dist package* angular.json
+git add ./dist package*
+git rm src/* jest* nest* angular* ts* zeladplay.swagger.json
 git commit --no-verify -m "Auto deploy from travis $TRAVIS_BUILD_NUMBER: ${rev}"
 git remote add deploy "https://jmcdo29:$GH_TOKEN@github.com/jmcdo29/zeldaplay.git" > /dev/null 2>&1
 git checkout -B build
