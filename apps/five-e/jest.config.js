5ae79507d08c649aab016fe6e1850ddaecd290d4
Jay McDoniel <jmcdo29@gmail.com>
2019-05-12 11:45:05 -0700

    removed the apps in favor of lazy loaded modules.

--- apps/five-e/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'five-e',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/five-e/',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- apps/four-e/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'four-e',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/four-e/',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
