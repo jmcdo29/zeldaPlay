7a880466a3b472927f3aa6d1dc14d805e908fca0
Jay McDoniel <jmcdo29@gmail.com>
2019-05-24 10:29:52 -0700

    Finished base of tables for now.

---
+++ scripts/bin/create-sql.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+cd ./scripts/sql
+
+touch migration.sql
+echo "-- Database migration script made from all the sql files created during development, made to be run in the appropriate order." > migration.sql
+echo . >> migration.sql
+
+echo "-- Databases to be used." >> migration.sql
+for i in $(ls database/); do
+  cat $i >> migration.sql
+done;
+
+echo . >> migration.sql
+echo "-- Extensions to be used." >> migration.sql
+
+for i in $(ls extenstions); do
+  cat $i >> migration.sql
+done;
+
+echo . >> migration.sql
+echo "-- Tables to be used." >> migration.sql
+
+for i in $(ls tables); do
+  cat $i >> migration.sql
+done;
--- scripts/bin/pre-commit.sh
+++ scripts/bin/pre-commit.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 npm run lint
-npm run format:check | npm run format
+npm run format:check || npm run format
 bash scripts/bin/gitAddMod.sh
