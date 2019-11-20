ee6335fcce8aa99a5afec216f9e8f527b181407f
Jay McDoniel <jmcdo29@gmail.com>
2019-11-19 20:40:26 -0800

    fix(session): update deserializer

--- apps/api/src/app/auth/session.serializer.ts
+++ apps/api/src/app/auth/session.serializer.ts
@@ -2,9 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { PassportSerializer } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 import { GoogleUser } from '../user/models/google-user.model';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
+  constructor(private readonly userService: UserService) {
+    super();
+  }
+
   serializeUser(
     userObs: Observable<GoogleUser>,
     done: (err: Error, user: any) => void,
@@ -19,8 +24,13 @@ export class SessionSerializer extends PassportSerializer {
     });
   }
 
-  deserializeUser(payload: any, done: (err: Error, user: any) => void) {
-    console.log(payload);
-    done(null, payload);
+  deserializeUser(
+    payload: { id: string; email: string },
+    done: (err: Error, user: any) => void,
+  ) {
+    this.userService.getById({ id: payload.id }).subscribe({
+      next: (user) => done(null, { id: user.id, email: user.email }),
+      error: (err) => done(err, null),
+    });
   }
 }
