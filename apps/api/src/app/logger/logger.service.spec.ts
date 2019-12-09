64dd32e17e2581d554d640d03675283a1f90b762
Jay McDoniel <jmcdo29@gmail.com>
2019-12-08 23:49:34 -0800

    refactor(logger): updates logger spec to use desccribe and it .each
    Jest's describe.each and it.each allow to take in an array of parameters to allow for dynamic
    reusable tests without having to rewrite most of the logic again.

--- apps/api/src/app/logger/logger.service.spec.ts
+++ apps/api/src/app/logger/logger.service.spec.ts
@@ -7,127 +7,59 @@ jest.mock('mc-scribe').enableAutomock();
 const shouldLog = (level: string): string => `Should log ${level}`;
 const stackTrace = 'Stack Trace';
 
-describe('LoggerService (no context)', () => {
+describe.each([
+  [
+    {
+      provide: LOGGER_MODULE_OPTIONS,
+      useValue: {
+        context: '',
+      },
+    },
+  ],
+  [
+    {
+      provide: LOGGER_MODULE_OPTIONS,
+      useValue: {
+        context: '',
+      },
+    },
+  ],
+])('LoggerModule LoggerService', (loggerOptions) => {
   let service: LoggerService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [LoggerService],
-    }).compile();
-    service = await module.resolve<LoggerService>(LoggerService);
-  });
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-  it('should have an blank context', () => {
-    expect((service as any).context).toBeFalsy();
-  });
-  it('should be able to log', () => {
-    service.log('something');
-  });
-});
-describe('LoggerModule LoggerService', () => {
-  let service: LoggerService;
-
-  beforeAll(async () => {
-    const module = await Test.createTestingModule({
-      providers: [
-        LoggerService,
-        {
-          provide: LOGGER_MODULE_OPTIONS,
-          useValue: {
-            context: 'Test',
-          },
-        },
-      ],
+      providers: [LoggerService, loggerOptions],
     }).compile();
     service = await module.resolve<LoggerService>(LoggerService);
   });
   it('should have a defined service', () => {
     expect(service).toBeDefined();
-    expect((service as any).context).toBeTruthy();
+    expect((service as any).context).toBe(loggerOptions.useValue.context);
   });
-  describe('dev env', () => {
-    beforeAll(() => {
-      process.env.NODE_ENV = 'dev';
-    });
-    describe('without context', () => {
-      it(shouldLog('error'), () => {
-        service.error('Error!', stackTrace);
+  describe.each([['dev'], ['development'], ['prod'], ['production']])(
+    '%s env',
+    (env) => {
+      beforeAll(() => {
+        process.env.NODE_ENV = env;
       });
-      it(shouldLog('log'), () => {
-        service.log('logs here');
+      describe.each([['TEST'], ['']])('%s context', (context: string) => {
+        it.each([
+          ['log', 'logs here'],
+          ['warn', 'warning!'],
+          ['debug', 'Debug'],
+          ['debug', { debug: 'DEBUG' }],
+          ['verbose', 'verbose!'],
+        ])(shouldLog('%s'), (logLevel: string, logString: any) => {
+          service[logLevel](logString, context);
+        });
+        it.each([
+          ['Error!', stackTrace],
+          ['Error!', ''],
+        ])(shouldLog('error'), (logString, stack) => {
+          service.error(logString, stack, context);
+        });
       });
-      it(shouldLog('warn'), () => {
-        service.warn('Warning!');
-      });
-      it(shouldLog('debug'), () => {
-        service.debug('Debug');
-        service.debug({ debug: 'DEBUG' });
-      });
-      it(shouldLog('verbose'), () => {
-        service.verbose('verbose!');
-      });
-    });
-    describe('with context', () => {
-      it(shouldLog('error'), () => {
-        service.error('Error!', stackTrace, 'TEST');
-      });
-      it(shouldLog('log'), () => {
-        service.log('logs here', 'TEST');
-      });
-      it(shouldLog('warn'), () => {
-        service.warn('Warning!', 'TEST');
-      });
-      it(shouldLog('debug'), () => {
-        service.debug('Debug', 'TEST');
-        service.debug({ debug: 'DEBUG' }, 'TEST');
-      });
-      it(shouldLog('verbose'), () => {
-        service.verbose('verbose!', 'TEST');
-      });
-    });
-  });
-  describe('development env', () => {
-    beforeAll(() => {
-      process.env.NODE_ENV = 'development';
-    });
-    it(shouldLog('error'), () => {
-      service.error('Error!', stackTrace);
-    });
-    it(shouldLog('log'), () => {
-      service.log('logs here', 'Test');
-    });
-    it(shouldLog('warn'), () => {
-      service.warn('Warning!');
-    });
-    it(shouldLog('debug'), () => {
-      service.debug('Debug');
-      service.debug({ debug: 'DEBUG' });
-    });
-    it(shouldLog('verbose'), () => {
-      service.verbose('verbose!');
-    });
-  });
-  describe('prod env', () => {
-    beforeAll(() => {
-      process.env.NODE_ENV = 'production';
-    });
-    it(shouldLog('error'), () => {
-      service.error('Error!', stackTrace);
-      service.error('ERROR!', '');
-    });
-    it(shouldLog('log'), () => {
-      service.log('logs here', 'Test');
-    });
-    it(shouldLog('warn'), () => {
-      service.warn('Warning!');
-    });
-    it(shouldLog('debug'), () => {
-      service.debug('Debug');
-    });
-    it(shouldLog('verbose'), () => {
-      service.verbose('verbose!');
-    });
-  });
+    },
+  );
 });
