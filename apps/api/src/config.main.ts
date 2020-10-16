571238767c5fac4b44849831dd4e4eb63aa922e2
Jay McDoniel <jmcdo29@gmail.com>
2020-10-16 05:55:22 -0700

    build: migrate from ng to nx and workspace.json

--- apps/api/src/config.main.ts
+++ apps/api/src/config.main.ts
@@ -1,7 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { OgmaService } from '@ogma/nestjs-module';
 import * as compression from 'compression';
-import * as cookie from 'cookie-parser';
 import * as helmet from 'helmet';
 import { ConfigService } from './app/config/config.service';
 
@@ -10,7 +9,7 @@ export function configure(
   config: ConfigService,
   logger: OgmaService,
 ): void {
-  app.use(helmet(), compression(), cookie());
+  app.use(helmet(), compression());
   app.setGlobalPrefix(config.globalPrefix);
   logger.log('Application Configuration complete', 'ApplicationConfig');
 }
